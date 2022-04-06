e63e67b6dc10b42bb523b79ce713ac64e601223c
lxmnet <lxmnet@outlook.com>
2022-04-06 17:06:30 +0800

    add icon

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
   // 站点配置
   lang: 'zh-CN',
   title: 'Note of Longdada',
-  head: [['link', { rel: 'icon', href: '/images/logo.png' }]],
+  head: [['link', { rel: 'icon', href: 'https://longdada.me/favicon.ico' }]],
   // 主题和它的配置
   theme: '@vuepress/theme-default',
   themeConfig: {
